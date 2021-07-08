e8f1e3157b7c2053f895d4fe9360a13624c01d64
jylls <jylls>
2021-07-08 11:31:57 -0400

    changing input and output paths

--- data.h
+++ data.h
@@ -7,9 +7,9 @@ using namespace std;
 /********************************************************************/
     const unsigned int n = 8;                          // number of spin variables
 
-    const string datafilename = "INPUT/toy_data.dat";	// Input datafile
-    const string basis_IntegerRepresentation_filename = "INPUT/notreal.dat";		// Input basis file
-    const string basis_BinaryRepresentation_filename = "INPUT/notreal.dat";		// Input basis file
+    const string datafilename = "toy_data.dat";	// Input datafile
+    const string basis_IntegerRepresentation_filename = "notreal.dat";		// Input basis file
+    const string basis_BinaryRepresentation_filename = "notreal.dat";		// Input basis file
 
     const string OUTPUT_directory = "OUTPUT/";
 
